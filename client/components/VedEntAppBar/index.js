2140480e37855489741fdbd5e264d781c00fb167
Ganesh Gitte <ganeshs.gitte911@gmail.com>
2021-08-10 11:27:25 +0530

    Navigation

--- client/components/VedEntAppBar/index.js
+++ client/components/VedEntAppBar/index.js
@@ -1,9 +1,18 @@
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 
+import { green, pink } from "@material-ui/core/colors";
+
+import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import ExpandIcon from "@material-ui/icons/ExpandMore";
+
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import { borderRadius } from "@material-ui/system";
 
 const useStyles = makeStyles((theme) => ({
   toolbar: {
@@ -22,6 +31,24 @@ const useStyles = makeStyles((theme) => ({
       fontSize: "1.5em",
     },
   },
+  navContainer: {
+    [theme.breakpoints.down("xs")]: {
+      display: "none",
+    },
+  },
+  navButton: {
+    color: "white",
+    fontWeight: 600,
+    padding: theme.spacing(1, 2),
+    margin: theme.spacing(0, 1),
+    backgroundColor: pink.A400,
+    borderRadius: theme.spacing(1),
+    border: "none",
+    "&:hover": {
+      backgroundColor: green[400],
+      color: "#FFF",
+    },
+  },
   menuButton: {
     margin: theme.spacing(1),
     [theme.breakpoints.up("sm")]: {
@@ -33,11 +60,77 @@ const useStyles = makeStyles((theme) => ({
 const VedEntAppBar = () => {
   const classes = useStyles();
 
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <nav>
       <AppBar position="fixed">
         <Toolbar className={classes.toolbar}>
           <div className={classes.title}>Ved Ent</div>
+          <div className={classes.navContainer}>
+            <Button variant="contained" className={classes.navButton}>
+              Home
+            </Button>
+            <Button variant="contained" className={classes.navButton}>
+              About
+            </Button>
+            <Button
+              variant="contained"
+              className={classes.navButton}
+              endIcon={<ExpandIcon />}
+              aria-controls="services-menu"
+              aria-haspopup="true"
+              onClick={handleOpen}
+            >
+              Services
+            </Button>
+            <Button variant="contained" className={classes.navButton}>
+              Gallary
+            </Button>
+            <Button variant="contained" className={classes.navButton}>
+              Contact
+            </Button>
+          </div>
+          <Menu
+            id="services-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={open}
+            onClose={handleClose}
+            getContentAnchorEl={null}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "center",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "center",
+            }}
+          >
+            <MenuItem>Carpentry Works</MenuItem>
+            <MenuItem>Civil Works</MenuItem>
+            <MenuItem>Commercial Cleaning</MenuItem>
+            <MenuItem>Corporate Technical</MenuItem>
+            <MenuItem>Electrical Works</MenuItem>
+            <MenuItem>Fabrication Works</MenuItem>
+            <MenuItem>Flooring Sales</MenuItem>
+            <MenuItem>Gardening and Lawn Management</MenuItem>
+            <MenuItem>Indusrial Flooring</MenuItem>
+            <MenuItem>Interior Design</MenuItem>
+            <MenuItem>Painting Works</MenuItem>
+            <MenuItem>Plumbing Works</MenuItem>
+            <MenuItem>Road and Public Area Cleaning</MenuItem>
+            <MenuItem>Wondow Blind Sales</MenuItem>
+          </Menu>
           <IconButton
             edge="end"
             className={classes.menuButton}
